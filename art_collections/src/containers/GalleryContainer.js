4dedc3376c4ccd7b46def756a220ddbb678e1a57
Yongran <maiyongran@gmail.com>
2022-10-06 16:19:00 +0100

    create GalleryContainer content

--- art_collections/src/containers/GalleryContainer.js
+++ art_collections/src/containers/GalleryContainer.js
@@ -1,3 +1,25 @@
-const GalleryContainer = () => {}
+const GalleryContainer = () => {
+   return (
+    <>
+			<header className="gallery-container-header">
+					<h1>Art Gallery</h1>
+					<h3>Collection</h3>
+			</header>
+			<main>
+				<section className="welcome-section">
+					<p>Welcome, feel free to dive yourself to the art world!</p>
+				</section>
+				<section className="collection-section">
+					<h3>Collections</h3>
+					<button>See All Collections</button>
+				</section>
+			</main>
+			<footer className="gallery-container-footer">
+				<h2>Sign up</h2>
+				<button>Sign Up</button>
+			</footer>
+    </>
+   )
+}
 
 export default GalleryContainer
