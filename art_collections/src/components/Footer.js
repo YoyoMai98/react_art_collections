b094016b0b061ff6e2f4cb3d020edac3f1b675c2
Yongran <maiyongran@gmail.com>
2022-10-06 22:07:24 +0100

    add media query

--- art_collections/src/components/Footer.js
+++ art_collections/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <footer className="gallery-container-footer">
-				<h2>Sign up</h2>
+				<h2>Do you wanna here more? Sign up for emails!</h2>
 				<Link to='/signup'>
                     <button>Sign Up</button>
                 </Link>
